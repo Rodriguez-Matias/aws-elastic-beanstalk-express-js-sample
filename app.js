93594b0910b850bbdca62fe826a8ccd5f36046c8
Matias-R <mrmatiasrodriguez@gmail.com>
2021-08-16 12:28:12 -0300

    modify the text in the webapp - test the pipeline configured in aws

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('My first pipelineproject at AWS!!!!!!!'));
+app.get('/', (req, res) => res.send('My first pipelineproject at AWS - another change!!!!!!!'));
 
-app.get('/', (req, res) => res.send('another change'));
+
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
