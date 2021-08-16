342107652c2a653b52fbec8a6f483236118499f4
Matias-R <mrmatiasrodriguez@gmail.com>
2021-08-16 12:24:08 -0300

    add another line to test the pipeline configured in aws

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ const port = 8080;
 
 app.get('/', (req, res) => res.send('My first pipelineproject at AWS!!!!!!!'));
 
+app.get('/', (req, res) => res.send('another change'));
+
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
